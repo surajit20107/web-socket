d6f8a28be13d6f8f9c1d3e3fa20b3fa6bf538bc4
Surajit <surajitjana20107@gmail.com>
2026-06-09 11:45:33 +0530

    init: intialize git and create websocket server

---
+++ server.js
@@ -0,0 +1,29 @@
+import http from "node:http";
+import { WebSocketServer } from "ws";
+import path from "path";
+import fs from "node:fs/promises";
+
+const port = process.env.PORT ?? 3000;
+
+const httpServer = http.createServer(async (req, res) => {
+  const index = await fs.readFile(path.resolve("./index.html"), "utf-8");
+  res.setHeader("Content-Type", "text/html");
+  return res.end(index);
+});
+
+const webSocket = new WebSocketServer({ server: httpServer });
+
+webSocket.on("connection", (websocket) => {
+  console.log("WebSocket connection...");
+
+  websocket.on("message", (data) => {
+    console.log("websocket message:", data.toString());
+    webSocket.clients.forEach((client) => {
+      client.send(data.toString());
+    });
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
